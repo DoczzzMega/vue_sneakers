ed8913337eb00c5ec2bb377d752bde0d04234023
DoczzzMega <roma.gunko87@gmail.com>
2025-01-29 00:45:00 +0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+import MyComponent from '@/components/MyComponent.vue'
+
+</script>
+
+<template>
+
+  <div class="w-4/5">
+
+    <h1>sdfsdfs</h1>
+
+  </div>
+
+</template>
+
+<style scoped>
+
+
+
+</style>
---
+++ src/components/MyComponent.vue
@@ -0,0 +1,37 @@
+<script setup>
+
+  defineProps({
+    isAdded: Boolean,
+  })
+
+  import {ref} from 'vue';
+
+  const count = ref(0);
+
+  const inc = () => count.value++;
+
+  const updateCount = (event) => count.value = Number(event.target.value);
+
+</script>
+
+
+<template>
+
+  <p>{{ isAdded }}</p>
+
+  <h1 class="text text-slate-500">{{ count }}</h1>
+  <button @click="inc">+</button>
+  <input type="text" @input="updateCount" />
+
+  <input type="text" v-model="count" placeholder="v-model" />
+
+</template>
+
+
+<style scoped>
+
+.text {
+  font-size: 40px;
+}
+
+</style>
